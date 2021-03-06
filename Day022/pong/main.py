55440c31eedcd5398ec590903195cd29e0938d19
silentnear@gmail.com <silentnear@gmail.com>
2021-03-06 20:59:21 +0800

    # 197. Create a Paddle that responds to Key Presses

---
+++ Day022/pong/main.py
@@ -0,0 +1,16 @@
+# 196. Set up the Main Screen
+# 197. Create a Paddle that responds to Key Presses
+
+from turtle import Screen, Turtle
+
+
+screen = Screen()
+screen.setup(width = 800, height = 600)
+screen.bgcolor("black")
+screen.title("Pong")
+
+
+
+
+
+screen.exitonclick()
